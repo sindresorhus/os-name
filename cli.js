ebf5f7bca3cc3f01dff8c33f0a3b19aef505d249
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-21 21:11:00 +0200

    use `meow` in the CLI

--- cli.js
+++ cli.js
@@ -1,28 +1,14 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var osName = require('./');
-var argv = process.argv;
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    os-name',
-		'    OS X Mavericks'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  os-name',
+		'  OS X Mavericks'
+	].join('\n')
+});
 
 console.log(osName());
